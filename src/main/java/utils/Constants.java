683f86558408107d47bce01c5d4a9aeb77596cd4
Cristina Apostol <cristina.apostol93@gmail.com>
2025-03-27 20:20:00 +0200

    Code updates

--- src/main/java/utils/Constants.java
+++
@@ -1,6 +0,0 @@
-package utils;
-
-public class Constants {
-        public static final String ERROR_USERNAME_REQUIRED = "Epic sadface: Username is required";
-        public static final String ERROR_PASSWORD_REQUIRED = "Epic sadface: Password is required";
-    }
